cd9b1469b493c88b54a1c12aa31690d017cd689d
devganesh91 <dev.pandian@tracxn.com>
2023-03-02 09:18:16 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 // Random change to trigger eslint workflow 7
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
     return (
         <div className='App'>
